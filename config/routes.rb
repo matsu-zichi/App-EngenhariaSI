ce4befe2bfb58d4556ba7e163800181565f69527
Rodrigo Birocchi <rodrigomarcelo@usp.br>
2022-10-12 18:50:53 -0300

    Usuários com devise configurado

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # root 'application#hello'
+  
+  devise_for :users
+  
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+
   root 'home#index'
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
