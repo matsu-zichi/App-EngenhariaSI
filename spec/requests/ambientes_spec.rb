56bb5db07db8ccf7af313eeb91eefa58bf3d102f
Thiago Matsumoto Zichi <thiago.siqui@usp.br>
2022-10-23 18:45:13 -0300

    fix codigo do spec

--- spec/requests/ambientes_spec.rb
+++ spec/requests/ambientes_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "/ambientes", type: :request do
-  
+
+  let(:valid_attributes) {
+    skip("Add a hash of attributes valid for your model")
+  }
+
   describe "GET /index" do
     it "renders a successful response" do
       Ambiente.create! valid_attributes
