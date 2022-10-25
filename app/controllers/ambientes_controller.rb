03d18d47aa314ad8afd5dfa3d679edbd67cea9ce
Thiago Matsumoto Zichi <thiago.siqui@usp.br>
2022-10-25 19:44:22 -0300

    operacoes CRUDI para Ambientes

--- app/controllers/ambientes_controller.rb
+++ app/controllers/ambientes_controller.rb
@@ -1,8 +1,16 @@
 class AmbientesController < ApplicationController
-
+  before_action :set_ambiente, only: %i[ show edit update destroy ]
   # GET
   def new
     @ambiente = Ambiente.new
+  end
+  
+  def show
+    @ambiente = Ambiente.find(params[:id])
+  end
+
+  def edit
+
   end
 
   # POST
@@ -11,7 +19,7 @@ class AmbientesController < ApplicationController
 
     respond_to do |format|
       if @ambiente.save
-        format.html { redirect_to ambiente_url(@ambiente), notice: "Ambiente was successfully created." }
+        format.html { redirect_to ambiente_url(@ambiente), notice: "Ambiente criado com sucesso." }
         format.json { render :show, status: :created, location: @ambiente }
       else
         # render :new, status: :unprocessable_entity, content_type: "text/html"
@@ -22,18 +30,41 @@ class AmbientesController < ApplicationController
     end
   end
 
-  
-  def show
+  def destroy
     @ambiente = Ambiente.find(params[:id])
+    if @ambiente.present?
+      @ambiente.destroy
+      respond_to do |format|
+        format.html { redirect_to ambientes_url, notice: "Ambiente deletado com sucesso." }
+        format.json { head :no_content }
+      end
+    end
   end
+
+  def update
+    respond_to do |format|
+      if @ambiente.update(ambiente_params)
+        format.html { redirect_to ambiente_url(@ambiente), notice: "Ambiente atualizado com sucesso." }
+        format.json { render :show, status: :ok, location: @ambiente }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @ambiente.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
 
   def index
     @ambientes = Ambiente.all
   end
 
   private
-  def ambiente_params
-    params.require(:ambiente).permit(:nome, :descricao)
-  end
-
+    # Use callbacks to share common setup or constraints between actions.
+    def set_ambiente
+      @ambiente = Ambiente.find(params[:id])
+    end
+    def ambiente_params
+      params.require(:ambiente).permit(:nome, :descricao)
+    end
 end
