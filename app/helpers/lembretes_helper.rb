890fd1c09f871e259a89196247060ec869e7bac3
VictorKeiji <victorkkissaba@usp.br>
2022-11-19 18:51:38 -0300

    countdown em dias horas minutos e segundos

--- app/helpers/lembretes_helper.rb
+++ app/helpers/lembretes_helper.rb
@@ -1,2 +1,21 @@
 module LembretesHelper
+    def calcula_countdown(segundos)
+        dias = horas = minutos = 0
+
+        if(segundos > 86400)
+            dias, segundos = segundos/86400, segundos%86400
+        end
+        if(segundos > 3600)
+            horas, segundos = segundos/3600, segundos%3600
+        end
+        if(segundos > 60)
+            minutos, segundos= segundos/60, segundos%60
+        end
+        if(segundos <= 0)
+            return "Expirado"
+        end
+
+        total = "%i dias, %i horas, %i minutos e %i segundos" % [dias, horas, minutos, segundos]
+        return total
+    end
 end
