cd7b508589fcb98d5ae87bf15b8a8a06fd1e06b5
VictorKeiji <victorkkissaba@usp.br>
2022-12-07 17:56:46 -0300

    melhoria cobertura mailer

--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -1,5 +1,26 @@
 require "rails_helper"
 
 RSpec.describe OrderMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'lembrete expirado' do
+    @user = FactoryBot.create(:user, email: "teste@teste.br", password: "123456", password_confirmation: "123456")
+    @user.skip_confirmation!
+    @user.save!
+    @ambiente = FactoryBot.create(:ambiente, nome: 'Teste', descricao: "", user: @user)
+    @ambiente.save!
+    @lembrete = Lembrete.first_or_create!({
+      :titulo => "Lembrete Email",
+      :texto => "teste notificação",
+      :ambiente_id => @ambiente.id,
+      :expire_at => 1.second.from_now,
+      :email => @user.email,
+      :prioridade => "P1"
+    })
+    @lembrete.save!
+
+    expect { 
+      OrderMailer.with(lembrete: @lembrete).expired_lembrete.deliver_later(wait_until: @lembrete.expire_at)
+    }.to(have_enqueued_job.at(@lembrete.expire_at))
+
+  end
+
 end
